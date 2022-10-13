583540d16d86f11bbb2318120f40ad831907278c
baselshabana <baselshabana@aucegypt.edu>
2022-10-13 16:48:35 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,39 @@
-import './App.css';
+import "./App.css";
 // import Container from 'react-bootstrap/Container'
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
 import Board from "./Game";
+import { updateURLParameter } from "./helpers";
+import { useState, useEffect } from "react";
 
 function App() {
-  
+  const [imageURL, setImageURL] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("img")) {
+      setImageURL(urlParams.get("img"));
+    }
+  }, []);
+
+  const handleImageChange = (e) => {
+    setImageURL(e.target.value);
+    window.history.replaceState(
+      "",
+      "",
+      updateURLParameter(window.location.href, "img", e.target.value)
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1>15-Puzzle Game</h1>
-      <Board  />
+        <h1>15-Puzzle Game</h1>
+        <Board imageURL={imageURL} />
+
       </header>
     </div>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //     <Board>
-
-  //     </Board>
-      
-      
-  //     {/* <div>
-  //       <Container>
-  //       <Row>
-  //         <Col>1</Col>
-  //         <Col>2</Col>
-  //         <Col>3</Col>
-  //         <Col>4</Col>
-  //       </Row>
-  //       <Row>
-  //         <Col>5</Col>
-  //         <Col>6</Col>
-  //         <Col>7</Col>
-  //         <Col>8</Col>
-  //       </Row>
-  //       <Row>
-  //         <Col>9</Col>
-  //         <Col>10</Col>
-  //         <Col>11</Col>
-  //         <Col>12</Col>
-  //       </Row>
-  //       <Row>
-  //         <Col>13</Col>
-  //         <Col>14</Col>
-  //         <Col>15</Col>
-  //         <Col>"</Col>
-  //       </Row>
-  //       </Container>
-  //     </div> */}
-  //     </header>
-  //   </div>
-  // );
+  
 }
 
 export default App;
--- src/Game.js
+++ src/Game.js
@@ -1,64 +1,61 @@
 import React, { useState } from "react";
 import Tile from "./Tile";
-import { TILE_COUNT, GRID_SIZE, BOARD_SIZE,SHUFFLE_NUM } from "./helpers"
-import {canSwap, swap, Won } from "./helpers"
+import { TILE_COUNT, GRID_SIZE, BOARD_SIZE, SHUFFLE_NUM } from "./helpers";
+import { canSwap, swap, Won } from "./helpers";
 
-var count =0;
-let listOfTiles= [];
+var count = 0;
+let listOfTiles = [];
 
-function Board({}) {
+function Board({ imageURL }) {
   const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()]);
   listOfTiles.splice(count, count, tiles);
 
+  const hasWon = Won(tiles);
 
-
-  const hasWon = Won(tiles)
-  
   const swapTiles = (tileIndex) => {
     if (canSwap(tileIndex, tiles.indexOf(tiles.length - 1)) && !hasWon) {
-      const swappedTiles = swap(tiles, tileIndex, tiles.indexOf(tiles.length - 1));
-      setTiles(swappedTiles)
-      count ++;
+      const swappedTiles = swap(
+        tiles,
+        tileIndex,
+        tiles.indexOf(tiles.length - 1)
+      );
+      setTiles(swappedTiles);
+      count++;
       listOfTiles.splice(count, count, swappedTiles);
     }
-  }
-
+  };
 
   const undo = () => {
-    if(listOfTiles[count-1] ){
-    count= count -1;
-    setTiles(listOfTiles[count])
+    if (listOfTiles[count - 1]) {
+      count = count - 1;
+      setTiles(listOfTiles[count]);
     }
-  }
+  };
 
   const shuffle = (tiles) => {
-    var rand = tiles[Math.floor(Math.random()*tiles.length)];
+    var rand = tiles[Math.floor(Math.random() * tiles.length)];
 
-    while ( !(canSwap( rand, tiles.indexOf(tiles.length - 1)))) {
-      rand = tiles[Math.floor(Math.random()*tiles.length)];
+    while (!canSwap(rand, tiles.indexOf(tiles.length - 1))) {
+      rand = tiles[Math.floor(Math.random() * tiles.length)];
     }
     if (canSwap(rand, tiles.indexOf(tiles.length - 1))) {
       const swappedTiles = swap(tiles, rand, tiles.indexOf(tiles.length - 1));
-      return(swappedTiles);
+      return swappedTiles;
     }
-    
-  }
-  
- 
+  };
+
   const handleTileClick = (index) => {
-    swapTiles(index)
-  }
-
-
+    swapTiles(index);
+  };
 
   const shuffleTiles = () => {
-    let swappedTiles= [...Array(TILE_COUNT).keys()];
+    let swappedTiles = [...Array(TILE_COUNT).keys()];
 
-    for(var i = 0; i< SHUFFLE_NUM ; i++ ){
+    for (var i = 0; i < SHUFFLE_NUM; i++) {
       swappedTiles = shuffle(swappedTiles);
     }
     setTiles(swappedTiles);
-  }
+  };
 
   const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
   const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE);
@@ -69,14 +66,14 @@ function Board({}) {
 
   return (
     <>
+      <button onClick={() => shuffleTiles()}>New game</button>
+
       <ul style={style} className="board">
-
-      <button onClick={() => shuffleTiles() }>New game</button>
-
         {tiles.map((tile, index) => (
           <Tile
             key={tile}
             index={index}
+            imageURL={imageURL}
             tile={tile}
             width={pieceWidth}
             height={pieceHeight}
@@ -84,11 +81,13 @@ function Board({}) {
           />
         ))}
       </ul>
-      <button onClick={() => undo() }>undo</button>
 
-      { hasWon && <div> CONGRATS!!🎉</div> }
+      {hasWon ? (
+        <div> CONGRATS!!🎉</div>
+      ) : (
+        <button onClick={() => undo()}>undo</button>
+      )}
     </>
-
   );
 }
 
